bc542e144534bcf26cd5a6e652c563c24b3d51cd
YehudaBenaish <135022723+YehudaBenaish@users.noreply.github.com>
2024-06-03 21:43:45 +0300

    ChangeLearnMore

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-
 // delete comment
 
-import './App.css';
-import WaterPage from './components/screens/WaterPage.js';
-import ContactUs from './components/screens/ContactPage.js';
-import Staff from './components/screens/StaffPage.js';
-import Students from './components/screens/StudentsPage.js';
-import Header from './components/MainHeader.js';
-import Footer from './components/Footer.js';
-import MainPage from './components/screens/MainPage.js';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import WaterPage from "./components/screens/WaterPage.js";
+import MicroClimaticPage from "./components/screens/MicroClimaticPage.js";
+import PowerPage from "./components/screens/PowerPage.js";
+import ContactUs from "./components/screens/ContactPage.js";
+import Staff from "./components/screens/StaffPage.js";
+import Students from "./components/screens/StudentsPage.js";
+import Header from "./components/MainHeader.js";
+import Footer from "./components/Footer.js";
+import MainPage from "./components/screens/MainPage.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/water" element={<WaterPage />} />
-        <Route path="/students" element={<Students />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/staff" element={<Staff />} />
-
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/water" element={<WaterPage />} />
+          <Route path="/power" element={<PowerPage />} />
+          <Route path="/microClimatic" element={<MicroClimaticPage />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/staff" element={<Staff />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
