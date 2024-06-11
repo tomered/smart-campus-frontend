845c4f270a827737becaa3f28938e02f2ee84bfe
morandeporto <morandeporto15@gmail.com>
2024-06-11 15:20:22 +0300

    Navbar Style and animation
    Worked with Eden

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,38 +1,40 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { FaBars } from 'react-icons/fa';
-import '../Style.css';
-import logoImage from '../hit_logo.svg';
+import '../Style.css'; // Ensure this file doesn't conflict with the styled-components
+import logoImage from '../HIT.png';
 
 const MainHeader = () => {
   const [showMenu, setShowMenu] = useState(false);
+
   const handleMenuClick = () => {
     setShowMenu(!showMenu);
   };
+
   return (
     <Container>
       <CustomNavBar>
-        {/*<img src={logoImage} alt="Smart Campus Logo" />*/}
-        <Title>
-          <a href='/'>Smart Campus</a>
-        </Title>
+        <LogoAndTitle href='/'>
+          <Logo src={logoImage} alt='Smart Campus Logo' />
+          <Title>Smart Campus</Title>
+        </LogoAndTitle>
         <MenuIcon onClick={handleMenuClick}>
           <FaBars />
         </MenuIcon>
         <MenuLinks show={showMenu}>
           <ul>
-            <li>
+            <MenuItem>
               <a href='Staff'>Staff</a>
-            </li>
-            <li>
+            </MenuItem>
+            <MenuItem>
               <a href='Students'>Students</a>
-            </li>
-            <li>
+            </MenuItem>
+            <MenuItem>
               <a href='Partners'>Partners</a>
-            </li>
-            <li>
+            </MenuItem>
+            <MenuItem>
               <a href='Contact'>Contact Us</a>
-            </li>
+            </MenuItem>
           </ul>
         </MenuLinks>
       </CustomNavBar>
@@ -42,22 +44,45 @@ const MainHeader = () => {
 
 export default MainHeader;
 
+const bounceAnimation = keyframes`
+  16.65% {
+    transform: translateY(8px);
+  }
+  33.3% {
+    transform: translateY(-6px);
+  }
+  49.95% {
+    transform: translateY(4px);
+  }
+  66.6% {
+    transform: translateY(-2px);
+  }
+  83.25% {
+    transform: translateY(1px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
 const Container = styled.header`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: #f5f5f5;
-  padding: 20px;
+  background-color: rgb(48 48 48);
+  padding: 5px 20px;
   border-bottom: 1px solid #dddddd;
   position: sticky;
   top: 0px;
   z-index: 1;
+  text-align: center;
+  color: white;
 
   h1 {
     font-family: 'Montserrat', sans-serif;
-    font-size: 32px;
-    color: #333333;
+    font-size: 24px;
+    color: white; /* White text color */
   }
 `;
 
@@ -66,29 +91,37 @@ const CustomNavBar = styled.nav`
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  text-transform: uppercase;
 
-  /* Media queries */
   @media screen and (max-width: 480px) {
     flex-direction: column;
   }
 `;
 
-const Logo = styled.div`
+const LogoAndTitle = styled.a`
   display: flex;
   align-items: center;
+  text-decoration: none; /* Ensure the link has no underline */
+
+  @media screen and (max-width: 480px) {
+    display: none;
+  }
+`;
+
+const Logo = styled.img`
+  width: 60px; /* Adjust the size as needed */
+  height: auto;
+  margin-bottom: 5px;
+  margin-right: 10px;
 `;
 
 const Title = styled.h1`
   font-family: 'Montserrat', sans-serif;
   font-size: 24px;
-  color: #333333;
+  color: white; /* White text color */
   margin-left: 10px;
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
+  margin: 0; /* Reset margin to ensure alignment with Logo */
 
-  /* Media queries */
   @media screen and (max-width: 480px) {
     display: none;
   }
@@ -98,7 +131,6 @@ const MenuIcon = styled.div`
   display: none;
   cursor: pointer;
 
-  /* Media queries */
   @media screen and (max-width: 480px) {
     display: block;
     font-size: 24px;
@@ -120,18 +152,18 @@ const MenuLinks = styled.div`
   }
 
   li {
-    margin-right: 20px;
+    margin-left: 3em; /* Match the left and right margins */
+    margin-right: 3em;
   }
 
   a {
-    color: #000000;
+    color: white; /* White text color */
     text-decoration: none;
     font-weight: bold;
     font-size: 18px;
-    transition: all 0.3s ease-in-out;
+    transition: color 0.5s ease-in-out;
   }
 
-  /* Media queries */
   @media screen and (max-width: 480px) {
     display: ${({ show }) => (show ? 'flex' : 'none')};
     flex-direction: column;
@@ -139,7 +171,51 @@ const MenuLinks = styled.div`
     top: 100%;
     left: 0;
     width: 100%;
-    background-color: #f5f5f5;
+    background-color: rgb(48, 48, 48);
     padding: 20px;
   }
+`;
+
+const hoverableStyles = css`
+  display: inline-block;
+  backface-visibility: hidden;
+  vertical-align: middle;
+  position: relative;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  transform: translateZ(0);
+  transition-duration: 0.3s;
+  transition-property: transform;
+
+  &:before {
+    position: absolute;
+    pointer-events: none;
+    z-index: -1;
+    content: '';
+    top: 100%;
+    left: 5%;
+    height: 10px;
+    width: 90%;
+    opacity: 0;
+    background: radial-gradient(
+      ellipse at center,
+      rgba(255, 255, 255, 0.35) 0%,
+      rgba(255, 255, 255, 0) 80%
+    );
+    transition-duration: 0.3s;
+    transition-property: transform, opacity;
+  }
+
+  &:hover,
+  &:active,
+  &:focus {
+    transform: translateY(-5px);
+
+    &:before {
+      opacity: 1;
+    }
+  }
+`;
+
+const MenuItem = styled.li`
+  ${hoverableStyles}
 `;
