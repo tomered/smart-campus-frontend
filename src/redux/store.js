d295dcdfc16b5ac695ee79b0fef142d88e0db71e
yuvalelarat <yuvalelarat@gmail.com>
2024-09-23 17:56:15 +0300

    done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userDataReducer from "./slices/userDataSlice";
 import { userDataApi } from "./rtk/userData";
-import { adminUsersApi } from "./rtk/adminUsersApi";
 
 export const store = configureStore({
   reducer: {
     userData: userDataReducer,
     [userDataApi.reducerPath]: userDataApi.reducer,
-    [adminUsersApi.reducerPath]: adminUsersApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       userDataApi.middleware,
-      adminUsersApi.middleware
     ),
 });
