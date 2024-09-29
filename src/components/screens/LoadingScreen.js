b6a6c032df9ef07802cd893bd3ea39c05ed38205
ofirharar <ofirharar6@gmail.com>
2024-09-29 09:55:22 +0300

    Update LoadingScreen.js
    Add animation to loading screen

--- src/components/screens/LoadingScreen.js
+++ src/components/screens/LoadingScreen.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import { CircularProgress, Box, Typography } from '@mui/material';
+import { keyframes } from '@emotion/react'; 
+
+
+const moveUpDown = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px); 
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
 
 const LoadingScreen = ({ message }) => {
   return (
@@ -28,10 +43,38 @@ const LoadingScreen = ({ message }) => {
       >
         <CircularProgress size={60} thickness={5} />
         <Typography sx={{ marginTop: '1rem', fontSize: '1.125rem', fontWeight: 600 }}>
-          {message}...
-        </Typography>
+        {message}
+        </Typography> 
+        <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '0.5rem' }}>
+          <Typography
+            sx={{
+              display: 'inline-block',
+              animation: `${moveUpDown} 1s ease-in-out infinite`,
+              marginRight: '0.1rem'
+            }}
+          >
+            .
+          </Typography>
+          <Typography
+            sx={{
+              display: 'inline-block',
+              animation: `${moveUpDown} 1s ease-in-out infinite 0.2s`,
+              marginRight: '0.1rem'
+            }}
+          >
+            .
+          </Typography>
+          <Typography
+            sx={{
+              display: 'inline-block',
+              animation: `${moveUpDown} 1s ease-in-out infinite 0.4s`,
+            }}
+          >
+            .
+          </Typography>
+        </Box>
       </Box>
-    </Box>
+    </Box> 
   );
 };
 
