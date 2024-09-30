1d59a8a1de69f39efd0b1e46dc1efb7c66dd1c0a
yuvalelarat <102436759+yuvalelarat@users.noreply.github.com>
2024-09-30 12:15:11 +0300

    Update userData.js

--- src/redux/rtk/userData.js
+++ src/redux/rtk/userData.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://localhost:10000/";
+const baseUrl = 'https://smart-campus-backend-4hd6.onrender.com/';
 
 export const userDataApi = createApi({
   reducerPath: "userDataApi",
