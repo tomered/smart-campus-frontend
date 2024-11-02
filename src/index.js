942afec2e49a46a80eefc3ca6b44b1b918ac10a6
tomered <125219614+tomered@users.noreply.github.com>
2024-11-02 22:38:30 +0200

    Revert "Add loading screen"

--- src/index.js
+++ src/index.js
@@ -4,17 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/es/integration/react'
-
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
