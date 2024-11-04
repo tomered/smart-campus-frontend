36b81686bec9f77032bf0e00aa0a64f8318a5a9a
yuvalelarat <yuvalelarat@gmail.com>
2024-11-04 23:17:26 +0200

    [bugFix]: navbar bug fix

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -58,6 +58,7 @@ const MainHeader = () => {
                     } else {
                       navigate(item.path);
                     }
+                    setShowMenu(false);
                   }}
                 >
                   {item.name}
