e4553774ff6771a7abbb1b9b457071386e1f0ebc
davidran1 <157808421+davidran1@users.noreply.github.com>
2024-11-16 18:07:19 +0200

    fix the bug that when login the loading screen is not above the login page

--- src/components/screens/LoginRegisterPage/LoginPage.js
+++ src/components/screens/LoginRegisterPage/LoginPage.js
@@ -56,12 +56,6 @@ const LoginPage = () => {
     setIsFailure(false);
   };
 
-  if(isLoading){
-    return(
-      <LoadingScreen message={'Verifying...'}/>
-    )
-  }
-
   return (
     <Container>
       <LoginForm onSubmit={handleSubmit}>
